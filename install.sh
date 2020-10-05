cbfd4bebe47f9283abd68f89f375abb8bd6a4f92
Yaoisss <3136586731@qq.com>
2020-10-05 18:53:08 +0800

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 docker pull iwebsec/iwebsec:latest
-docker pull area39/pikachu
+docker pull area39/pikachu:latest
 docker pull c0ny1/sqli-labs:0.1
-docekr pull c0ny1/upload-labs
+docker pull c0ny1/upload-labs:latest
+docker pull area39/webug:latest
